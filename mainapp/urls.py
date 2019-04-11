ffac123156a59b95b61642d9e06b7bc8cda574eb
Powiratel <48507849+Powiratel@users.noreply.github.com>
2019-04-11 21:11:20 +0300

    Add files via upload

---
+++ mainapp/urls.py
@@ -0,0 +1,92 @@
+"""geekshop URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
+
+from django.conf.urls import url
+from django.contrib import admin
+import mainapp.views as mainapp
+
+urlpatterns = [
+    url(r'^$', mainapp.main, name='index'),
+    url(r'^products/', mainapp.products, name='products'),
+    url(r'^contact/', mainapp.contacts, name='contacts'),
+    url(r'^admin/', admin.site.urls, name='admin'),
+]
+
+path('auth/', include('authapp.urls', namespace='auth'))
+
+from django.urls import path
+
+import authapp.views as authapp
+
+app_name = 'authapp'
+
+urlpatterns = [
+    path('login/', authapp.login, name='login'),
+    path('logout/', authapp.logout, name='logout'),
+]
+
+from django.conf import settings
+ from django.conf.urls.static import static
+
+ if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+from django.conf.urls import include
+
+urlpatterns = [
+    path('', mainapp.main, name='main'),
+    path('products/', include('mainapp.urls', namespace='products')),
+    path('contact/', mainapp.contact, name='contact'),
+    path('admin/', admin.site.urls),
+]
+
+from django.urls import path
+
+import mainapp.views as mainapp
+
+app_name = 'basketapp'
+
+urlpatterns = [
+   path('', mainapp.products, name='index'),
+   path('<int:pk>/', mainapp.products, name='category'),
+]
+from django.urls import path
+
+import mainapp.views as mainapp
+
+app_name = 'basketapp'
+
+urlpatterns = [
+    path('', mainapp.products, name='index'),
+    path('category/<int:pk>/', mainapp.products, name='category'),
+]
+from django.urls import path
+
+import mainapp.views as mainapp
+
+app_name = 'basketapp'
+
+urlpatterns = [
+    path('', mainapp.products, name='index'),
+    path('category/<int:pk>/', mainapp.products, name='category'),
+    path('product/<int:pk>/', mainapp.product, name='product'),
+]
---
+++ mainapp/views.py
@@ -0,0 +1,168 @@
+from django.shortcuts import render
+
+
+def main(request):
+    return render(request, 'mainapp/index.html', context={ 'user': {'name': 'daniil', 'surname':'Popov'}})
+
+
+def products(request):
+    return render(request, 'mainapp/products.html')
+
+
+def contacts(request):
+    return render(request, 'mainapp/contacts.html')
+
+
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib import auth
+from django.urls import reverse
+
+
+def login(request):
+    title = 'вход'
+
+    login_form = ShopUserLoginForm(data=request.POST)
+    if request.method == 'POST' and login_form.is_valid():
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+        if user and user.is_active:
+            auth.login(request, user)
+            return HttpResponseRedirect(reverse('main'))
+
+    content = {'title': title, 'login_form': login_form}
+
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('main'))
+
+
+from .models import ProductCategory, Product
+
+
+def main(request):
+    title = 'главная'
+
+    products = Product.objects.all()[:4]
+
+    content = {'title': title, 'products': products}
+    return render(request, 'mainapp/index.html', content)
+
+
+from django.shortcuts import get_object_or_404
+
+def products(request, pk=None):
+    print(pk)
+
+    title = 'продукты'
+    links_menu = ProductCategory.objects.all()
+
+    if pk is not None:
+        if pk == 0:
+            products = Product.objects.all().order_by('price')
+            category = {'name': 'все'}
+        else:
+            category = get_object_or_404(ProductCategory, pk=pk)
+            products = Product.objects.filter(category__pk=pk).order_by('price')
+
+        content = {
+            'title': title,
+            'links_menu': links_menu,
+            'category': category,
+            'products': products,
+        }
+
+        return render(request, 'mainapp/products_list.html', content)
+
+    same_products = Product.objects.all()[3:5]
+
+    content = {
+        'title': title,
+        'links_menu': links_menu,
+        'same_products': same_products
+    }
+    basket = []
+    if request.user.is_authenticated:
+        basket = Basket.objects.filter(user=request.user)
+
+    if pk:
+        if pk == '0':
+            products = Product.objects.all().order_by('price')
+            category = {'name': 'все'}
+        else:
+            category = get_object_or_404(ProductCategory, pk=pk)
+            products = Product.objects.filter(category__pk=pk).order_by('price')
+
+        content = {
+            'title': title,
+            'links_menu': links_menu,
+            'category': category,
+            'products': products,
+            'basket': basket,
+        }
+
+        return render(request, 'mainapp/products_list.html', content)
+
+    return render(request, 'mainapp/products.html', content)
+
+
+import random
+
+
+def get_basket(user):
+    if user.is_authenticated:
+        return Basket.objects.filter(user=user)
+    else:
+        return []
+
+
+def get_hot_product():
+    products = Product.objects.all()
+
+    return random.sample(list(products), 1)[0]
+
+
+def get_same_products(hot_product):
+    same_products = Product.objects.filter(category=hot_product.category). \
+                        exclude(pk=hot_product.pk)[:3]
+
+    return same_products
+
+
+...
+
+
+def products(request, pk=None):
+    title = 'продукты'
+    links_menu = ProductCategory.objects.all()
+    basket = get_basket(request.user)
+
+    ...
+
+    hot_product = get_hot_product()
+    same_products = get_same_products(hot_product)
+
+    content = {
+        'title': title,
+        'links_menu': links_menu,
+        'hot_product': hot_product,
+        'same_products': same_products,
+        'basket': basket,
+    }
+
+    return render(request, 'mainapp/products.html', content)
+
+
+def product(request, pk):
+    title = 'продукты'
+
+    content = {
+        'title': title,
+        'links_menu': ProductCategory.objects.all(),
+        'product': get_object_or_404(Product, pk=pk),
+        'basket': get_basket(request.user),
+    }
+
+    return render(request, 'mainapp/product.html', content)
